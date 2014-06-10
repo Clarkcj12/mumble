9a67d8574d426c2c93375ca763e29d7e5a8ac3df
Clarkcj12 <clarkcj@cox.net>
2014-06-10 17:52:23 -0400

    Create main.java

---
+++ src/me.Clarkcj.mumble/main.java
@@ -0,0 +1,36 @@
+package me.Clarkcj.mumble;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mumble extends JavaPlugin{
+	public final Logger logger = Logger.getLogger("Minecraft");
+    public static Mumble plugin;
+    
+    @Override
+    public void onDisable(){
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+    
+    }
+    @Override
+    public void onEnable(){
+    	PluginDescriptionFile pdfFile = this.getDescription();
+    	this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + "Has Been Enabled!");
+  
+    }
+    
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		if (commandLabel.equalsIgnoreCase("mumble")){
+			player.sendMessage(ChatColor.GOLD + "You can get mumble at http://mumble.sourceforge.net/!");
+		}
+		return false;
+	}
+}
