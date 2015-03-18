7b7993685f6371832f495ab3c89bf4b7b012100c
Corey Clark <Clarkcj12@users.noreply.github.com>
2015-03-17 23:41:31 -0400

    Update and rename main.java to Mumble.java
    Added Comment to indicate what it does, fixed console error if tried to run via console. And made a more efficient config call.

--- src/me.Clarkcj.mumble/Mumble.java
+++ src/me.Clarkcj.mumble/Mumble.java
@@ -1,11 +1,11 @@
 package me.Clarkcj.mumble;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,20 +21,25 @@ public class Mumble extends JavaPlugin{
     }
     @Override
     public void onEnable(){
+    	this.saveDefaultConfig();
     	PluginDescriptionFile pdfFile = this.getDescription();
     	this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + "Has Been Enabled!");
-    	getConfig().options().copyDefaults(true);
-    	saveConfig();
+    {try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
+    }
     }
     
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		Player player = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("mumble")){
-			player.sendMessage(ChatColor.GOLD + "Download Mumble: http://mumble.sourceforge.net/"); //send command
-		}else if (cmd.getName().equalsIgnoreCase("mumbleinfo")){
-			player.sendMessage(ChatColor.GOLD + "Mumble Server Address: " + getConfig().getString("Server"));
-			player.sendMessage(ChatColor.GOLD + "The Port: " + getConfig().getString("Port"));
-			player.sendMessage(ChatColor.GOLD + "Server Password: " + getConfig().getString("Password"));
+			sender.sendMessage(ChatColor.GOLD + "Download Mumble: http://mumble.sourceforge.net/"); //Send Player Download Link
+		}else if (cmd.getName().equalsIgnoreCase("mumbleinfo")){ //If Run Give Server Info
+			sender.sendMessage(ChatColor.GOLD + "Mumble Server Address: " + getConfig().getString("Server"));
+			sender.sendMessage(ChatColor.GOLD + "The Port: " + getConfig().getString("Port"));
+			sender.sendMessage(ChatColor.GOLD + "Server Password: " + getConfig().getString("Password"));
 	        }
 		return false;
 	}
